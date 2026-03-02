34c2a6a04e190c3d576bd716344109b8955fcc78
imagar2025-web <imagar2025@gmail.com>
2026-03-02 18:00:04 +0100

    push a servidor docker

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended'
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'vue/multi-word-component-names': 'off'  // ← añade esto
   }
 }
